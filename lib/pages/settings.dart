f4b2329f1a1c487f5e2c435ff96eec031a79f661
Joselay <tongmenglaysmae5@gmail.com>
2024-07-16 17:14:55 +0700

    ...

---
+++ lib/pages/settings.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_ztm/providers/user_provider.dart';
+
+class Settings extends ConsumerStatefulWidget {
+  const Settings({super.key});
+
+  @override
+  ConsumerState<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends ConsumerState<Settings> {
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    LocalUser currentUser = ref.watch(userProvider);
+    _nameController.text = currentUser.user.name;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Settings"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "Enter Your Name",
+                ),
+                controller: _nameController,
+              ),
+              TextButton(
+                onPressed: () {
+                  ref
+                      .read(userProvider.notifier)
+                      .updateName(_nameController.text);
+                },
+                child: const Text("Update"),
+              )
+            ],
+          ),
+        ));
+  }
+}
